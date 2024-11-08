2a2d2dc1fdb72b4e12f614f29d604d44e95774df
demian <demian@koreainvestment.com>
2024-11-08 12:44:44 +0900

    06 과제

---
+++ 전슬마로/06/Memorize/MemorizeApp.swift
@@ -0,0 +1,18 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    @StateObject var game = EmojiMemoryGame()
+    
+    var body: some Scene {
+        WindowGroup {
+            EmojiMemoryGameView(viewModel: game)
+        }
+    }
+}
