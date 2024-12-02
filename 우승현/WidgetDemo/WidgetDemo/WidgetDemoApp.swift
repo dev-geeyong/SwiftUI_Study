9c739feb734a7540343072a682887f9314507ff2
Seunghyun Woo <seunghyunwoo99@gmail.com>
2024-12-03 00:12:29 +0900

    위젯데모 초기커밋

---
+++ 우승현/WidgetDemo/WidgetDemo/WidgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDemoApp.swift
+//  WidgetDemo
+//
+//  Created by 우승현 on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
