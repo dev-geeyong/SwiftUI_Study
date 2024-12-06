80615092bb87f9d3778a673540749d607c87a1f8
정다연 <diane@koreainvestment.com>
2024-12-06 13:54:03 +0900

    Lecture 9 프로젝트 생성

---
+++ 정다연/Memorize09/Memorize09/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize09
+//
+//  Created by 정다연 on 12/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 정다연/Memorize09/Memorize09/Memorize09App.swift
@@ -0,0 +1,19 @@
+//
+//  Memorize09App.swift
+//  Memorize09
+//
+//  Created by 정다연 on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Memorize09App: App {
+    @StateObject var game = EmojiMemoryGame()
+    
+    var body: some Scene {
+        WindowGroup {
+            EmojiMemoryGameView(viewModel: game)
+        }
+    }
+}
