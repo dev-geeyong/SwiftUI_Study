89c661ffd0f7134516511329edc625ef45bd19ad
정다연 <diane@koreainvestment.com>
2024-12-17 17:28:57 +0900

    구성 옵션 추가하기 - Location

--- 정다연/WidgetDemo/WeatherWidget/WeatherWidget.swift
+++ 정다연/WidgetDemo/WeatherWidget/WeatherWidget.swift
@@ -7,134 +7,130 @@
 
 import WidgetKit
 import SwiftUI
+import Intents
 
-struct Provider: AppIntentTimelineProvider {
+struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> WeatherEntry {
-        WeatherEntry(
-            date: Date(),
-            city: "London",
-            temperature: 89,
-            description: "Thunder Storm",
-            icon: "cloud.bolt.rain",
-            image: "thunder",
-            url: thunderUrl
-        )
+        WeatherEntry(date: Date(), city: "London",
+                           temperature: 89, description: "Thunder Storm",
+                                icon: "cloud.bolt.rain", image: "thunder",
+                                                    url: thunderUrl)
+
     }
 
-    func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> WeatherEntry {
-        let entry = WeatherEntry(
-            date: Date(),
-            city: "London",
-            temperature: 89,
-            description: "Thunder Storm",
-            icon: "cloud.bolt.rain",
-            image: "thunder",
-            url: thunderUrl
-        )
-        
-        return entry
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (WeatherEntry) -> ()) {
+        let entry = WeatherEntry(date: Date(), city: "London",
+                    temperature: 89, description: "Thunder Storm",
+                         icon: "cloud.bolt.rain", image: "thunder",
+                                 url: thunderUrl)
+        completion(entry)
     }
-    
-    func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<WeatherEntry> {
-        // 초기에 런던의 타임라인을 반환
-        // interval마다 콘텐츠를 변경
+
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        
+        var chosenLocation: LocationData
+            
+        if configuration.locations == .londonUK {
+            chosenLocation = .london
+        } else {
+            chosenLocation = .miami
+        }
+        
         var entries: [WeatherEntry] = []
         var eventDate = Date()
-        let interval: TimeInterval = 5
-        for var entry in londonTimeline {
+        let halfMinute: TimeInterval = 30
+    
+        for var entry in chosenLocation.timeline {
             entry.date = eventDate
-            eventDate += interval
+            eventDate += halfMinute
             entries.append(entry)
         }
-
-        // 첫번째 타임라인이 끌날 때 새로운 타임라인을 요청하지 않음
-        return Timeline(entries: entries, policy: .never)
+        let timeline = Timeline(entries: entries, policy: .never)
+        completion(timeline)
     }
 }
-
 
 struct WeatherWidgetEntryView : View {
     var entry: Provider.Entry
 
+    @Environment(\.widgetFamily) var widgetFamily
+
     var body: some View {
         ZStack {
-            WeatherSubView(entry: entry)
-        }
-        .containerBackground(for: .widget) {
             Color("weatherBackgroundColor")
+            HStack {
+                WeatherSubView(entry: entry)
+                if widgetFamily == .systemMedium {
+                    Image(entry.image)
+                        .resizable()
+                }
+            }
+            .containerBackground(for: .widget) {
+                Color("weatherBackgroundColor")
+            }
         }
         .widgetURL(entry.url)
     }
 }
 
-struct WeatherSubView: View { 
+struct WeatherSubView: View {
+    
     var entry: WeatherEntry
     
     var body: some View {
+        
         VStack {
             VStack {
                 Text("\(entry.city)")
                     .font(.title)
-                Image (systemName: entry.icon)
+                Image(systemName: entry.icon)
                     .font(.largeTitle)
                 Text("\(entry.description)")
                     .frame(minWidth: 125, minHeight: nil)
             }
             .padding(.bottom, 2)
-            .background(ContainerRelativeShape().fill(Color("weatherInsetColor")))
-            Label("\(entry.temperature)ºF", systemImage: "thermometer")
+            .background(ContainerRelativeShape()
+                       .fill(Color("weatherInsetColor")))
+            Label("\(entry.temperature)°F", systemImage: "thermometer")
         }
         .foregroundColor(.white)
         .padding()
     }
 }
 
+@main
 struct WeatherWidget: Widget {
     let kind: String = "WeatherWidget"
 
     var body: some WidgetConfiguration {
-        AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
             WeatherWidgetEntryView(entry: entry)
         }
+        .configurationDisplayName("My Weather Widget")
+        .description("A demo weather widget.")
+        .supportedFamilies([.systemSmall, .systemMedium])
     }
 }
 
 struct WeatherWidget_Previews: PreviewProvider {
     static var previews: some View {
-        WeatherWidgetEntryView(entry: WeatherEntry(date: Date(),
-                                                   city: "London",
-                                                   temperature: 89,
-                                                   description: "Thunder Storm",
-                                                   icon: "cloud.bolt.rain",
-                                                   image: "thunder",
-                                                   url: thunderUrl))
-        .previewContext(WidgetPreviewContext(family: .systemSmall))
+        
+        Group {
+            WeatherWidgetEntryView(entry: WeatherEntry(date: Date(),
+                                                       city: "London", temperature: 89,
+                                                       description: "Thunder Storm",
+                                                       icon: "cloud.bolt.rain", image: "thunder",
+                                                       url: thunderUrl))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    
+            WeatherWidgetEntryView(entry: WeatherEntry(date: Date(),
+                  city: "London", temperature: 89,
+                  description: "Thunder Storm", icon: "cloud.bolt.rain",
+                        image: "thunder", url: thunderUrl))
+                .previewContext(WidgetPreviewContext(
+                                         family: .systemMedium))
+            
+        }
         
     }
 }
-
-extension ConfigurationAppIntent {
-    fileprivate static var smiley: ConfigurationAppIntent {
-        let intent = ConfigurationAppIntent()
-        intent.favoriteEmoji = "😀"
-        return intent
-    }
-    
-    fileprivate static var starEyes: ConfigurationAppIntent {
-        let intent = ConfigurationAppIntent()
-        intent.favoriteEmoji = "🤩"
-        return intent
-    }
-}
-
-//struct SimpleEntry: TimelineEntry {
-//    let date: Date
-//    let configuration: ConfigurationAppIntent
-//}
-
-//#Preview(as: .systemSmall) {
-//    WeatherWidget()
-//} timeline: {
-//    SimpleEntry(date: .now, configuration: .smiley)
-//    SimpleEntry(date: .now, configuration: .starEyes)
-//}
